a59b30df937153879a8e8498aab49f0f9e0576ad
iktomy87 <tomyrodriguez1939@gmail.com>
2026-09-15 00:25:16 -0300

    fix

--- backend/dist/src/reportes/reportes.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response } from 'express';
-import { ReportesService } from './reportes.service';
-export declare class ReportesController {
-    private readonly service;
-    constructor(service: ReportesService);
-    descargarAsignacion(convocatoriaId: string, res: Response): Promise<void>;
-}
--- backend/dist/src/reportes/reportes.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { PrismaService } from '../prisma/prisma.service';
-export declare class ReportesService {
-    private readonly prisma;
-    constructor(prisma: PrismaService);
-    generarReporteAsignacion(convocatoriaId: string): Promise<string>;
-}
